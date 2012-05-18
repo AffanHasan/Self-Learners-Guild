9039f34a38e98796eb47e8a461bfb046d585120b
Affan Hasan <affan.hasan@hotmail.com>
2012-05-18 12:36:33 +0500

    1. Deleted BIEKSYPhysicsCH11.java 2. Added BIEKSYPhysicsCH11Numericals.java 3. Restructured the 'webapp' directory which is more logical

--- src/main/java/org/selfLearnersGuild/data/biek/physics/SYPhysics/ch11/BIEKSYPhysicsCh11Numericals.java
+++ src/main/java/org/selfLearnersGuild/data/biek/physics/SYPhysics/ch11/BIEKSYPhysicsCh11Numericals.java
@@ -1,8 +1,3 @@
-/**
- * This class is the backing bean for phySYCh11.xhtml
- * 
- * @author Affan Hasan
- */
 package org.selfLearnersGuild.data.biek.physics.SYPhysics.ch11;
 
 import java.io.Serializable;
@@ -12,21 +7,22 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @SessionScoped
-@Named("biekSYPhysicsChEleven")
-public class BIEKSYPhysicsCh11 implements Serializable {
+@Named("biekSYPhysicsChElevenNumericals")
+public class BIEKSYPhysicsCh11Numericals implements Serializable {
 
-	private static final long serialVersionUID = -2460441280488996123L;
+	private static final long serialVersionUID = -3782670526850949458L;
+	
 	private Integer stepNumber;
 	private String visitorNumericalSelection;
-	private BIEKSYPhysicsCh11.Examples examples;
-	private BIEKSYPhysicsCh11.Exercise exercise;
+	private BIEKSYPhysicsCh11Numericals.Examples examples;
+	private BIEKSYPhysicsCh11Numericals.Exercise exercise;
 
 	@PostConstruct
 	public void postConstructionInitiallize() {
 		this.stepNumber = null;
 		this.visitorNumericalSelection = null;
-		this.examples = new BIEKSYPhysicsCh11.Examples();
-		this.exercise = new BIEKSYPhysicsCh11.Exercise();
+		this.examples = new BIEKSYPhysicsCh11Numericals.Examples();
+		this.exercise = new BIEKSYPhysicsCh11Numericals.Exercise();
 	}
 
 	/**
@@ -46,18 +42,23 @@ public class BIEKSYPhysicsCh11 implements Serializable {
 		public boolean isSolvedExample11_1() {
 			return solvedExample11_1;
 		}
+		
 		public void setSolvedExample11_1(boolean solvedExample11_1) {
 			this.solvedExample11_1 = solvedExample11_1;
 		}
+		
 		public boolean isSolvedExample11_2() {
 			return solvedExample11_2;
 		}
+		
 		public void setSolvedExample11_2(boolean solvedExample11_2) {
 			this.solvedExample11_2 = solvedExample11_2;
 		}
+		
 		public boolean isSolvedExample11_3() {
 			return solvedExample11_3;
 		}
+		
 		public void setSolvedExample11_3(boolean solvedExample11_3) {
 			this.solvedExample11_3 = solvedExample11_3;
 		}
@@ -79,20 +80,45 @@ public class BIEKSYPhysicsCh11 implements Serializable {
 		public boolean isSolvedProblem11_1() {
 			return solvedProblem11_1;
 		}
+		
 		public void setSolvedProblem11_1(boolean solvedProblem11_1) {
 			this.solvedProblem11_1 = solvedProblem11_1;
 		}
+		
 		public boolean isSolvedProblem11_2() {
 			return solvedProblem11_2;
 		}
+		
 		public void setSolvedProblem11_2(boolean solvedProblem11_2) {
 			this.solvedProblem11_2 = solvedProblem11_2;
 		}
+		
 		public boolean isSolvedProblem11_3() {
 			return solvedProblem11_3;
 		}
+		
 		public void setSolvedProblem11_3(boolean solvedProblem11_3) {
 			this.solvedProblem11_3 = solvedProblem11_3;
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Till now the Main purpose of this inner class is only to hold the
+	 * 'solved' state of a numerical.
+	 * </p>
+	 * 
+	 * @author Affan Hasan
+	 */
+	public class Exams {
+		private boolean solvedRegular19951_C;
+
+		public boolean isSolvedRegular19951_C() {
+			return solvedRegular19951_C;
+		}
+		
+		public void setSolvedRegular19951_C(boolean solvedRegular19951_C) {
+			this.solvedRegular19951_C = solvedRegular19951_C;
 		}
 	}
 
@@ -116,25 +142,19 @@ public class BIEKSYPhysicsCh11 implements Serializable {
 		this.visitorNumericalSelection = visitorNumericalSelection;
 	}
 
-	public BIEKSYPhysicsCh11.Examples getExamples() {
+	public BIEKSYPhysicsCh11Numericals.Examples getExamples() {
 		return examples;
 	}
 
-	public void setExamples(BIEKSYPhysicsCh11.Examples examples) {
-		if (examples == null)
-			this.examples = new BIEKSYPhysicsCh11.Examples();
-		else
-			this.examples = examples;
+	public void setExamples(BIEKSYPhysicsCh11Numericals.Examples examples) {
+		this.examples = examples;
 	}
 
-	public BIEKSYPhysicsCh11.Exercise getExercise() {
+	public BIEKSYPhysicsCh11Numericals.Exercise getExercise() {
 		return exercise;
 	}
 
-	public void setExercise(BIEKSYPhysicsCh11.Exercise exercise) {
-		if(exercise == null)
-			this.exercise = null;
-		else
-			this.exercise = exercise;
-	}
+	public void setExercise(BIEKSYPhysicsCh11Numericals.Exercise exercise) {
+		this.exercise = exercise;
+	}	
 }
